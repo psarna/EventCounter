c5d315a0ce2dbaeeec4ad809d4fa5711748ef75f
Michał Kaźmierowski <kazik@skytechnology.pl>
2015-11-12 18:55:13 +0100

    Compilation working much.

--- src/event_counter.cc
+++ src/event_counter.cc
@@ -1,27 +1,50 @@
 #include "event_counter.h"
 
-void EventCounter::Result::add(const Event &event) {
-	this->min = std::min(this->min, event.value);
-	this->max = std::max(this->max, event.value);
+#include <cmath>
+
+template<int N, typename Key, typename Value>
+void EventCounter<N, Key, Value>::PartialResult::add(const Event &event) {
+	this->values.insert(event.value);
 	this->total += event.value;
 	this->count += 1;
 }
 
-
-
-void EventCounter::updatePeriod(const Event &event, int period) {
-	Results &results = partial_results_[period];
-	Results &next_results = partial_results_[period + 1];
-
-	results[event.key].remove(event);
-	next_results[event.key].add(event);
+template<int N, typename Key, typename Value>
+void EventCounter<N, Key, Value>::PartialResult::remove(const Event &event) {
+	this->values.erase(event.value);
+	this->total -= event.value;
+	this->count -= 1;
 }
 
-void EventCounter::registerEvent(const Key &key, Value value,
-		unsigned int /*timestamp*/) {
+
+template<int N, typename Key, typename Value>
+void EventCounter<N, Key, Value>::updatePeriod(const Events &events, int period) {
+	PartialResults &results = partial_results_[period];
+	PartialResults &next_results = partial_results_[period + 1];
+
+	for (const auto &event : events.data) {
+		results[event.key].remove(event);
+		next_results[event.key].add(event);
+	}
+}
+
+template<int N, typename Key, typename Value>
+void EventCounter<N, Key, Value>::registerEvent(const Key &key, Value value,
+		unsigned int timestamp) {
 	
+	if (timestamp = highest_timestamp_ + 1) {
+		for (int period = 0; period < (int)std::log2(N); period++) {
+			Events &events = queue_[1 << period];
+			updatePeriod(events, period);
+		}
+		queue_.push_front(Events{timestamp, {Event{key, value}}});
+		queue_.pop_back();
+	}
 }
 
 int main(void) {
-	
+	EventCounter<16> ec;	
+	ec.registerEvent(std::string("aa"), 3, 1);
+	ec.registerEvent(std::string("aa"), 3, 2);
+	ec.registerEvent(std::string("aa"), 3, 3);
 }
--- src/event_counter.h
+++ src/event_counter.h
@@ -2,9 +2,17 @@
 #define __EVENT_COUNTER_H
 
 #include <deque>
+#include <set>
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+struct Result {
+	long min;
+	long max;
+	long total;
+	long count;
+};
 
 template<int N, typename Key = std::string, typename Value = long>
 class EventCounter {
@@ -13,20 +21,25 @@ class EventCounter {
 		Value value;
 	};
 
-	struct Result {
-		long min;
-		long max;
-		long total;
+	struct Events {
+		unsigned int timestamp;
+		std::vector<Event> data;
+	};
+
+
+	struct PartialResult {
+		std::set<Value> values;
+		Value total;
 		long count;
 
 		void add(const Event &event);
 		void remove(const Event &event);
 	};
 
-typedef std::unordered_map<Key, Result> Results;
+typedef std::unordered_map<Key, PartialResult> PartialResults;
 
 public:
-	EventCounter() : queue_(N), partial_results_() {
+	EventCounter() : queue_(N+1), partial_results_(), highest_timestamp_() {
 	}
 
 	void registerEvent(const Key &key, Value value, unsigned int timestamp);
@@ -35,10 +48,11 @@ public:
 			unsigned int time_period, int n);
 
 private:
-	void updatePeriod(const Event &event, int period);
+	void updatePeriod(const Events &events, int period);
 
-	std::deque<Event> queue_;
-	std::array<Results, sizeof(N)> partial_results_;
+	unsigned int highest_timestamp_;
+	std::deque<Events> queue_;
+	std::array<PartialResults, sizeof(N)> partial_results_;
 };
 
 #endif
