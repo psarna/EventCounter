919b3689c42671761fd1d9859821864925fdfdf5
Piotr Sarna <p.sarna@tlen.pl>
2015-12-08 15:42:02 +0100

    working version with sliding min/max disabled

--- src/event_counter.h
+++ src/event_counter.h
@@ -31,8 +31,26 @@ public:
 			total(0), count(0) {
 		}
 
-		void add(const Result& result);
-		void remove(const Result& result);
+		void add(const Result& other) {
+			this->min = std::min(this->min, other.min);
+			this->max = std::max(this->max, other.max);
+			this->total += other.total;
+			this->count += other.count;
+		}
+
+
+		void remove(const Result& other) {
+			/* TODO: fixme
+			if (other.min == this->min) {
+				this->min = 0;
+			}
+			if (other.max == this->max) {
+				this->max = 0;
+			}*/
+			this->total -= other.total;
+			this->count -= other.count;
+		}
+
 		void print() const {
 			printf("(%s, %s, %ld, %ld)\n",
 				min == std::numeric_limits<Value>::max() ? "X" : std::to_string(min).c_str(),
@@ -42,33 +60,33 @@ public:
 		}
 	};
 
-	struct PartialResult : public Result {
+	struct AccumulatedResult : public Result {
 		typedef Sliding<Value, N/2, std::less<Value>> SlidingMax;
 		typedef Sliding<Value, N/2, std::greater<Value>> SlidingMin;
 
-		SlidingMin partial_min;
+		SlidingMin accumulated_min;
 		cbuf<typename SlidingMin::pointer, N+1> min_pointers;
-		SlidingMax partial_max;
+		SlidingMax accumulated_max;
 		cbuf<typename SlidingMax::pointer, N+1> max_pointers;
 
-		PartialResult(Value value) : Result(value) {
+		AccumulatedResult(Value value) : Result(value) {
 		}
 
-		PartialResult() : Result() {
+		AccumulatedResult() : Result() {
 		}
 
 		Value min() {
-			return partial_min.top();
+			return accumulated_min.top();
 		}
 
 		Value max() {
-			return partial_max.top();
+			return accumulated_max.top();
 		}
 
-		void add(const Result &result) {
-			auto min_ptr = partial_min.update(result.min);
+		/*void add(const Result &result) {
+			auto min_ptr = accumulated_min.update(result.min);
 			min_pointers.put(min_ptr);
-			auto max_ptr = partial_max.update(result.max);
+			auto max_ptr = accumulated_max.update(result.max);
 			max_pointers.put(max_ptr);
 			this->total += result.total;
 			this->count += result.count;
@@ -76,34 +94,106 @@ public:
 
 		void remove(const Result &result) {
 			auto min_ptr = min_pointers.get();
-			partial_min.erase(min_ptr);
+			accumulated_min.erase(min_ptr);
 			auto max_ptr = max_pointers.get();
-			partial_max.erase(max_ptr);
+			accumulated_max.erase(max_ptr);
 			this->total -= result.total;
 			this->count -= result.count;
-		}
+		}*/
 	};
 
-	EventCounter() : queue_(N), partial_results_(), highest_timestamp_() {
+	EventCounter() : queue_(N), accumulated_results_(), highest_timestamp_() {
 	}
 
-	void registerEvent(const KeyRepresentation &key, Value value, unsigned int timestamp);
-	void query(const KeyRepresentation &key, unsigned int time_period, Result &result);
+	void registerEvent(const KeyRepresentation &key_repr, Value value, unsigned int timestamp) {
+		std::lock_guard<Mutex> lock(mutex_);
+		Key key = key_map_.get(key_repr);
+		if (key == -1) {
+			key = key_map_.put(key_repr);
+			// first - invalidate old entries.
+			invalidateKey(key);
+		}
+		registerEvent(key, value, timestamp);
+	}
+
 	void getTopKeys(std::vector<std::pair<KeyRepresentation, Result>> &results,
 			unsigned int time_period, int n);
 
-	void registerEvent(const Key &key, Value value, unsigned int timestamp);
-	void query(const Key &key, unsigned int time_period, Result &result);
+	void registerEvent(const Key &key, Value value, unsigned int timestamp) {
+			// Case 1: Older event is registered
+			if (timestamp <= highest_timestamp_ ) {
+				// Edge case: Event is too old
+				if (highest_timestamp_ - timestamp >= N) {
+					printf("Ignoring old data: %d not in [%d, %d]\n", timestamp, highest_timestamp_ - N + 1, highest_timestamp_);
+					return;
+				}
+				// Regular case: Event is insterted into the structure
+				int queue_index = highest_timestamp_ - timestamp;
+				Result &target = queue_[queue_index][key];
+				target.add(Result(value));
+				// Update period with newly added value
+				addToPeriod(key, Result(value), log2(queue_index + 1));
+
+			}
+
+			// Case 2: Fresh event is registered
+			if (timestamp > highest_timestamp_) {
+				while (timestamp > highest_timestamp_ + 1) {
+					highest_timestamp_++;
+					// Add empty event to queue
+					queue_.put(EventCounter::Results{});
+					// Update partial results for all periods
+					for (int period = 1; period <= log2<N>(); period++) {
+						Results &results = queue_[(1 << period) - 1];
+						updatePeriod(results, period);
+					}
+					// Remove the oldest record from the queue
+					queue_.get();
+				}
+				assert(timestamp == highest_timestamp_ + 1);
+				// Add newly registered event to queue
+				queue_.put(EventCounter::newResults(key, value));
+				// Update partial results for first period
+				addToPeriod(key, Result(value), 0);
+				// Update partial results for all periods
+				for (int period = 1; period <= log2<N>(); period++) {
+					Results &results = queue_[(1 << period) - 1];
+					updatePeriod(results, period);
+				}
+				// Remove the oldest record from the queue
+				queue_.get();
+
+				highest_timestamp_ = timestamp;
+			}
+		}
+
+	void query(const KeyRepresentation &key_repr, unsigned int time_period, Result &result) {
+		std::lock_guard<Mutex> lock(mutex_);
+		Key key = key_map_.get(key_repr);
+		if (key == -1){
+			result = Result();
+		} else {
+			query(key, time_period, result);
+		}
+	}
+
+	void query(const Key &key, unsigned int time_period, Result &result) {
+		for (int i = time_period - 1; i >= 0; --i) {
+			AccumulatedResults &results = accumulated_results_[i];
+			result.add(results[key]);
+		}
+	}
+
 	void getTopKeys(std::vector<std::pair<Key, Result>> &results,
 			unsigned int time_period, int n);
 
 	void print(size_t max_len = 4) const {
 		printf("Event Counter (highest timestamp = %d) {\n", highest_timestamp_);
-		for (int i = 0; i < std::min(max_len, partial_results_.size()); ++i) {
+		for (int i = 0; i < std::min(max_len, accumulated_results_.size()); ++i) {
 			printf("\trange [%d, %d) : \n", (1 << i) - 1, (1 << (i + 1)) - 1);
-			for (int j = 0; j < partial_results_[i].size(); ++j) {
+			for (int j = 0; j < accumulated_results_[i].size(); ++j) {
 				printf("\t\t%d: ", j);
-				partial_results_[i][j].print();
+				accumulated_results_[i][j].print();
 			}
 			printf("\n");
 		}
@@ -116,172 +206,52 @@ public:
 	}
 
 private:
-	typedef std::array<PartialResult, KeyCount> PartialResults;
+	typedef std::array<AccumulatedResult, KeyCount> AccumulatedResults;
 	typedef std::array<Result, KeyCount> Results;
 
-	void updatePeriod(const PartialResults &results, int period);
-	void addToPeriod(const Key &key, const Result &result, int period);
-	void removeFromPeriod(const Key &key, const Result &result, int period);
-	void invalidateKey(const Key key);
+	void updatePeriod(const Results &results, int period) {
+		AccumulatedResults &curr_results = accumulated_results_[period - 1];
+		AccumulatedResults &next_results = accumulated_results_[period];
 
-	static PartialResults newResults(Key key, Value value) {
-		PartialResults new_results = {};
-		new_results[key] = {PartialResult(value)};
+		for (int i = 0; i < results.size(); ++i) {
+			const Result &result = results[i];
+			curr_results[i].remove(result);
+			if (period < log2<N>()) {
+				next_results[i].add(result);
+			}
+		}
+	}
+
+	void addToPeriod(const Key &key, const Result &result, int period) {
+		AccumulatedResults &curr_results = accumulated_results_[period];
+
+		curr_results[key].add(result);
+	}
+
+	void removeFromPeriod(const Key &key, const Result &result, int period);
+
+	void invalidateKey(const Key key) {
+		// remove from queue
+		for (int i = queue_.start; i != queue_.end; i = (i + 1) % queue_.size) {
+			queue_[i][key] = Result();
+		}
+		// remove from partial_results
+		for (auto &accumulated : accumulated_results_) {
+			accumulated[key] = AccumulatedResult();
+		}
+	}
+
+	static Results newResults(Key key, Value value) {
+		Results new_results = {};
+		new_results[key] = {Result(value)};
 		return new_results;
 	}
 
 	unsigned int highest_timestamp_;
 	cbuf<Results, N+1> queue_;
-	std::array<PartialResults, 8 * sizeof(N)> partial_results_;
+	std::array<AccumulatedResults, 8 * sizeof(N)> accumulated_results_;
 	Immortal<fixed_map<KeyRepresentation, Key, KeyCount>> key_map_;
 	Mutex mutex_;
 };
-
-// Implementation
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::Result::add(const Result &other) {
-	this->min = std::min(this->min, other.min);
-	this->max = std::max(this->max, other.max);
-	this->total += other.total;
-	this->count += other.count;
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::Result::remove(const Result &other) {
-	/* TODO: fixme
-	if (other.min == this->min) {
-		this->min = 0;
-	}
-	if (other.max == this->max) {
-		this->max = 0;
-	}*/
-	this->total -= other.total;
-	this->count -= other.count;
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::addToPeriod(
-		const Key &key, const Result &result, int period) {
-	PartialResults &curr_results = partial_results_[period];
-
-	curr_results[key].add(result);
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::updatePeriod(
-		const PartialResults &results, int period) {
-	PartialResults &curr_results = partial_results_[period - 1];
-	PartialResults &next_results = partial_results_[period];
-
-	for (int i = 0; i < results.size(); ++i) {
-		const Result &result = results[i];
-		curr_results[i].remove(result);
-		if (period < log2<N>()) {
-			next_results[i].add(result);
-		}
-	}
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::invalidateKey(const Key key) {
-	// remove from queue
-	for (int i = queue_.start; i != queue_.end; i = (i + 1) % queue_.size) {
-		queue_[i][key] = Result();
-	}
-	// remove from partial_results
-	for (auto &partial : partial_results_) {
-		partial[key] = Result();
-	}
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::registerEvent(
-		const KeyRepresentation &key_repr, Value value, unsigned int timestamp) {
-	std::lock_guard<Mutex> lock(mutex_);
-	Key key = key_map_.get(key_repr);
-	if (key == -1) {
-		key = key_map_.put(key_repr);
-		// first - invalidate old entries.
-		invalidateKey(key);
-	}
-	registerEvent(key, value, timestamp);
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::registerEvent(
-		const Key &key, Value value, unsigned int timestamp) {
-	// Case 1: Older event is registered
-	if (timestamp <= highest_timestamp_ ) {
-		// Edge case: Event is too old
-		if (highest_timestamp_ - timestamp >= N) {
-			printf("Ignoring old data: %d not in [%d, %d]\n", timestamp, highest_timestamp_ - N + 1, highest_timestamp_);
-			return;
-		}
-		// Regular case: Event is insterted into the structure
-		int queue_index = highest_timestamp_ - timestamp;
-		Result &target = queue_[queue_index][key];
-		target.add(Result(value));
-		// Update period with newly added value
-		addToPeriod(key, Result(value), log2(queue_index + 1));
-
-	}
-
-	// Case 2: Fresh event is registered
-	if (timestamp > highest_timestamp_) {
-		while (timestamp > highest_timestamp_ + 1) {
-			highest_timestamp_++;
-			// Add empty event to queue
-			queue_.put(EventCounter::Results{});
-			// Update partial results for all periods
-			for (int period = 1; period <= log2<N>(); period++) {
-				Results &results = queue_[(1 << period) - 1];
-				updatePeriod(results, period);
-			}
-			// Remove the oldest record from the queue
-			queue_.get();
-		}
-		//printf("timestamp = %u, highest_timestamp = %u, %u\n", timestamp, highest_timestamp_, highest_timestamp_ + 1);
-		//assert(timestamp == highest_timestamp_ + 1);
-		// Add newly registered event to queue
-		queue_.put(EventCounter::newResults(key, value));
-		// Update partial results for first period
-		addToPeriod(key, Result(value), 0);
-		// Update partial results for all periods
-		for (int period = 1; period <= log2<N>(); period++) {
-			Results &results = queue_[(1 << period) - 1];
-			updatePeriod(results, period);
-		}
-		// Remove the oldest record from the queue
-		queue_.get();
-
-		highest_timestamp_ = timestamp;
-	}
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::query(
-		const KeyRepresentation &key_repr, unsigned int time_period, Result &result) {
-	std::lock_guard<Mutex> lock(mutex_);
-	Key key = key_map_.get(key_repr);
-	if (key == -1){
-		result = Result();
-	} else {
-		query(key, time_period, result);
-	}
-}
-
-template<int N, int KeyCount, typename KeyRepresentation, typename Key, typename Value>
-void EventCounter<N, KeyCount, KeyRepresentation, Key, Value>::query(
-		const Key &key, unsigned int time_period, Result &result) {
-	for (int i = time_period - 1; i >= 0; --i) {
-		Results &results = partial_results_[i];
-		result.add(results[key]);
-	}
-}
-
-
-
-
 
 #endif
